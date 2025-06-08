18d4b7790028828ea130f0d595cd07d5af44e1c8
JeanCarpio <berthasuarez459@gmail.com>
2025-06-08 14:39:54 -0500

    Update Habito.js

--- code/Habito.js
+++ code/Habito.js
@@ -1,1 +1,11 @@
-"use strict";
+export class Habito {
+  constructor(
+    public id: number,
+    public nombre: string,
+    public cumplimientos: Date[] = []
+  ) {}
+
+  registrarCumplimiento(fecha: Date): void {
+    this.cumplimientos.push(fecha);
+  }
+}
